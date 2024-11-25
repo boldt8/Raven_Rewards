504070287fab646121dd34f70acc348c5297bc50
Alexander Boldt <aboldt4@Alexanders-MacBook-Air.local>
2024-11-25 01:14:33 -0800

    Created QR scanner

---
+++ Raven Rewards/Controllers/Core Tabs/QRScanner.swift
@@ -0,0 +1,18 @@
+//
+//  QRScanner.swift
+//  Raven Rewards
+//
+//  Created by Alexander Boldt on 11/25/24.
+//
+
+import SwiftUI
+
+struct QRScanner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QRScanner()
+}
